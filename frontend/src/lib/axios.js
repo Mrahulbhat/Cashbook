814131199dd6b04624352e96d4314941d5c7084d
Mrahulbhat <rahulbhat5122@gmail.com>
2025-12-28 18:57:14 +0530

    updated frontend url and pakcage.json

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.MODE === "development"
-    ? "http://localhost:5001/api":'',
-    // : "https://servicecare.onrender.com/api",
+    ? "http://localhost:5001/api"
+    : "https://cashbook-ee9a.onrender.com/api",
   withCredentials: true,
 });
