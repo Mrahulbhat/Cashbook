5a93b886dd19e3f3e643334b70b1745e9c24365f
mrahulbhat <rahulbhat5122@gmail.com>
2026-04-03 18:00:23 +0530

    updated auto

--- automation/page-objects/transaction.ts
+++ automation/page-objects/transaction.ts
@@ -73,59 +73,7 @@ export class TransactionPage extends BasePage {
 
     }
 
-    async editTransaction(page: Page, transaction: { type: string, amount: string, accountName: string, categoryName: string, date: string, description: string }) {
-
-        const initialTxnCountText = await this.recordCountOnTable.innerText();
-
-        await this.editButton.first().click();
-        await Promise.all([
-            page.waitForResponse((response: any) => response.url().includes(commonConstants.urls.transactionAPI) && response.status() === 200)
-        ]);
-
-        const updated_type = 'Income';
-        const updated_amount = '2000';
-        const updated_accountName = 'Canara Bank';
-        const updated_categoryName = 'Salary';
-        const updated_date = '2025-12-30';
-        const updated_description = 'TEST AUTOMATION - EDITED';
-
-        // assert default selection is expense
-        await expect(this.expenseRadio).toBeChecked();
-
-        await this.incomeRadio.click();
-        await expect(this.incomeRadio).toBeChecked();
-        await this.enterAmount(updated_amount);
-        await this.selectAccount(updated_accountName);
-        await this.selectCategory(updated_categoryName);
-        await this.selectDate(updated_date);
-        await this.descriptionInput.fill(updated_description);
-        await expect(this.cancelButton).toBeVisible();
-        await expect(this.saveButton).toBeEnabled();
-        await this.saveButton.click();
-
-        await Promise.all([
-            page.waitForResponse((response: any) => response.url().includes(commonConstants.urls.transactionAPI) && response.status() === 200, { timeout: 15000 }),
-            expect(page.getByText(commonConstants.toastMessages.TRANSACTION_UPDATED_SUCCESSFULLY)).toBeVisible()
-        ]);
-
-        // Verify if it stays on the same page and count remains the same
-        await expect(this.addButton).toBeVisible();
-        const postCreationTxnCountText = await this.recordCountOnTable.innerText();
-        expect(parseInt(postCreationTxnCountText)).toBe(parseInt(initialTxnCountText));
-
-        // Verify if all details are correct in the latest transaction row
-        await expect(this.firstRowOfGrid).toContainText(updated_type.toLowerCase());
-        await expect(this.firstRowOfGrid).toContainText(updated_categoryName);
-        await expect(this.firstRowOfGrid).toContainText(updated_accountName);
-        await expect(this.firstRowOfGrid).toContainText(`₹${Number(updated_amount).toLocaleString('en-IN')}`);
-        await expect(this.firstRowOfGrid).toContainText(updated_description);
-        const formattedDate1 = new Date(updated_date).toLocaleDateString("en-US", {
-            year: "numeric",
-            month: "short",
-            day: "numeric",
-        });
-        await expect(this.firstRowOfGrid).toContainText(formattedDate1);
-    };
+    
 
     async deleteAllTransactions(page: Page) {
         const initialTxnCountText = await this.recordCountOnTable.innerText();
--- automation/tests/transaction.spec.ts
+++ automation/tests/transaction.spec.ts
@@ -1,4 +1,6 @@
+import commonConstants from '../constants/commonConstants.js';
 import { test } from '../fixtures/test-base.js';
+import { expect } from '@playwright/test';
 
 test.describe('Transaction Related Tests', () => {
     test.beforeEach(async ({ loginPage }) => {
@@ -9,31 +11,79 @@ test.describe('Transaction Related Tests', () => {
         await settingsPage.wipeData();
     });
 
-    test('Create a Transaction @BAT', async ({ page, transactionPage,accountsPage,categoryPage ,settingsPage}) => {
-    
-        // Create a Category
+    test('Transaction CRUD Operations @BAT', async ({ page, transactionPage, accountsPage, categoryPage, settingsPage }) => {
+
+        // Declare constants
         const category = { name: "Food", type: "expense", parentCategory: "Needs", budget: "1000" };
-        await categoryPage.createCategory(page, category);
-
-        // Create an Account
         const account = { name: "CANARA_BANK", balance: "1000" };
-        await accountsPage.createAccount(page, account);
-
-        // Create a Transaction
         const transaction = { amount: "1000", type: "expense", accountName: "CANARA_BANK", categoryName: "Food", date: new Date().toISOString().split('T')[0], description: 'TEST AUTOMATION' };
+
+
+        const updatedCategory = { name: "Salary", type: "income", parentCategory: "Income" };
+        const updatedAccount = { name: "Cash", balance: "2000" };
+        const updatedTransaction = { amount: "2000", type: "income", accountName: "Cash", categoryName: "Salary", date: new Date().toISOString().split('T')[0], description: 'TEST AUTOMATION - EDITED' };
+
+
+        // Create Category, Account and Transaction
+        await categoryPage.createCategory(page, category);
+        await categoryPage.createCategory(page, updatedCategory);
+        await accountsPage.createAccount(page, account);
+        await accountsPage.createAccount(page, updatedAccount);
         await transactionPage.createTransaction(page, transaction);
 
+        // Edit a Transaction
+
+        const initialTxnCountText = await transactionPage.recordCountOnTable.innerText();
+
+        await transactionPage.editButton.first().click();
+        await Promise.all([
+            page.waitForResponse((response: any) => response.url().includes(commonConstants.urls.transactionAPI) && response.status() === 200)
+        ]);
+
+        // assert default selection is expense
+        await expect(transactionPage.expenseRadio).toBeChecked();
+
+        await transactionPage.incomeRadio.click();
+        await expect(transactionPage.incomeRadio).toBeChecked();
+        await transactionPage.enterAmount(updatedTransaction.amount);
+        await transactionPage.selectAccount(updatedTransaction.accountName);
+        await transactionPage.selectCategory(updatedTransaction.categoryName);
+        await transactionPage.selectDate(updatedTransaction.date);
+        await transactionPage.descriptionInput.fill(updatedTransaction.description);
+        await expect(transactionPage.cancelButton).toBeVisible();
+        await expect(transactionPage.saveButton).toBeEnabled();
+        await transactionPage.saveButton.click();
+
+        await Promise.all([
+            page.waitForResponse((response: any) => response.url().includes(commonConstants.urls.transactionAPI) && response.status() === 200, { timeout: 15000 }),
+            expect(page.getByText(commonConstants.toastMessages.TRANSACTION_UPDATED_SUCCESSFULLY)).toBeVisible()
+        ]);
+
+        // Verify if it stays on the same page and count remains the same
+        await expect(transactionPage.addButton).toBeVisible();
+        const postCreationTxnCountText = await transactionPage.recordCountOnTable.innerText();
+        expect(parseInt(postCreationTxnCountText)).toBe(parseInt(initialTxnCountText));
+
+        // Verify if all details are correct in the latest transaction row
+        await expect(transactionPage.firstRowOfGrid).toContainText(updatedTransaction.type.toLowerCase());
+        await expect(transactionPage.firstRowOfGrid).toContainText(updatedTransaction.categoryName);
+        await expect(transactionPage.firstRowOfGrid).toContainText(updatedTransaction.accountName);
+        await expect(transactionPage.firstRowOfGrid).toContainText(`₹${Number(updatedTransaction.amount).toLocaleString('en-IN')}`);
+        
+        // Match the actual UI date format (Short date)
+        const expectedUIDate = new Date(updatedTransaction.date).toLocaleDateString();
+        await expect(transactionPage.firstRowOfGrid).toContainText(expectedUIDate);
+
+        // Delete Transaction
+        await transactionPage.deleteAllTransactions(page);
     });
 
-    test('Transaction CRUD Operation @BAT @BUG', async ({ page, transactionPage, dashboardPage }) => {
-
-        const transaction = { amount: "1000", type: "income", accountName: "CANARA_BANK", categoryName: "TEST1", date: '2025-12-29', description: 'TEST AUTOMATION' };
-
-        await transactionPage.createTransaction(page, transaction);
-
-
-        // EDIT THE RECORD===================================================================================
-
-        await transactionPage.editTransaction(page, transaction);
-    });
+    /*
+    1. Verify the display of records in the transaction grid
+    2. Edit transaction functionality
+    3. Delete transaction functionality
+    4. Delete all transactions functionality
+    5. Verify if account can be created from Transaction dropdown
+    6. Verify if category can be created from Transaction dropdown
+    */
 });
